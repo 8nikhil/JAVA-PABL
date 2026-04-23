70e538db22d1884a184e45fea85c11e58b5ed01c
NIKHIL <8nikhil282005@gmail.com>
2026-04-23 21:27:43 +0530

    Create Program_11.java

---
+++ Program_11.java
@@ -0,0 +1,40 @@
+class Solution {
+    
+    public int kthSmallest(int[] arr, int k) {
+        return quickSelect(arr, 0, arr.length - 1, k - 1);
+    }
+
+    private int quickSelect(int[] arr, int left, int right, int k) {
+        if (left == right) return arr[left];
+
+        int pivotIndex = partition(arr, left, right);
+
+        if (k == pivotIndex) {
+            return arr[k];
+        } else if (k < pivotIndex) {
+            return quickSelect(arr, left, pivotIndex - 1, k);
+        } else {
+            return quickSelect(arr, pivotIndex + 1, right, k);
+        }
+    }
+
+    private int partition(int[] arr, int left, int right) {
+        int pivot = arr[right];
+        int i = left;
+
+        for (int j = left; j < right; j++) {
+            if (arr[j] <= pivot) {
+                swap(arr, i, j);
+                i++;
+            }
+        }
+        swap(arr, i, right);
+        return i;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
